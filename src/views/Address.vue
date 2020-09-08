be60ea291dd782ca7c5a8559c2d96bc62732c7cb
Wouter van Dam <wouter.van.dam@gmail.com>
2020-09-08 16:23:36 +0200

    Add Mapbox to Address page

--- src/views/Address.vue
+++ src/views/Address.vue
@@ -2,18 +2,18 @@
   <Page :step="1" :steps="8" class="Address">
     
     <div class="Address__Wrapper">
-      <div class="Address--left">
-        <Title>
-          Melding maken voor het adres:
-        </Title>
-        
-        <Form :busy="busy">
-          <FormField v-model="address" label="Zoek het adres" id="address" :valid="valid" @validate="handleValidation" />
-        </Form>
-
-      </div>
+      <Title>
+        Melding maken voor het adres:
+      </Title>
+      
+      <Form :busy="busy">
+        <FormField v-model="address" label="Zoek het adres" id="address" :valid="valid" @validate="handleValidation" />
+      </Form>
       <!-- Replace with Mapbox / Google Map + position without borders: remove padding from wrapper + apply to left -->
-      <img class="Address--right" src="../assets/home.jpg" width="640" />
+      
+    </div>
+    <div class="MapBox__Wrapper">
+      <MapBox :accessToken="token" :mapStyle="style" />
     </div>
 
     <template slot="footer">
@@ -37,11 +37,14 @@ import SvgIcon from '@/components/common/SvgIcon.vue'
 import Form from '@/components/common/Form.vue'
 import FormField from '@/components/common/FormField.vue'
 
+import MapBox from '@/components/common/MapBox.vue'
+
 
 @Component({
   components: {
     Page, Button, SvgIcon, Title,
-    Form, FormField
+    Form, FormField,
+    MapBox
   }
 })
 export default class Address extends Vue {
@@ -52,8 +55,12 @@ export default class Address extends Vue {
 
   private busy = false;
 
+  private token = process.env.VUE_APP_MAPBOX_TOKEN;
+  private style = process.env.VUE_APP_MAPBOX_STYLE;
+
+
   /**
-   * TODO: A strong longer than 1 character will do...
+   * TODO: A string longer than 1 character will do...
    */
   handleValidation(value: string|number|boolean|Array<string>) {
     this.valid = ((value + '').trim().length > 1)
@@ -86,17 +93,20 @@ export default class Address extends Vue {
 
 <style lang="scss">
 .Address {
+
+  .Page__Content {
+    padding: 0;
+    flex-direction: row;
+    justify-content: space-between;
+  }
+
   &__Wrapper {
     display: flex;
+    flex-direction: column;
     max-width: 100%;
     justify-content: space-between;
-
-    img {
-      max-width: 640px;
-    }
-  }
-  &--left {
-    max-width: calc(100% - 720px);
+    height: 100%;
+    padding: 50px 80px;
 
     .Title, .BodyText {
       margin-bottom: 26px;
@@ -104,6 +114,11 @@ export default class Address extends Vue {
     .Button {
       margin-top: 24px;
     }
+  }
+  .MapBox__Wrapper {
+    position: relative;
+    max-width: 758px;
+    width: 758px;
   }
 }
 </style>
