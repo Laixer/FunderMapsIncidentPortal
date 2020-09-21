75e79236c46facf4eeca78957e3b9a60c92ef412
Patrick Nobbe <patrick.nobbe@hotmail.com>
2020-09-21 18:44:59 +0200

    Update github actions script

--- .github/workflows/build-vendors.sh
+++ .github/workflows/build-vendors.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+function build() {
+    npm run build -- --vendor=$1
+    git describe --long --always > $1-dist/VERSION
+    git rev-parse HEAD > $1-dist/COMMIT
+    tar -czf $1-dist.tar.gz -C $1-dist .
+}
+
 for f in ./vendors/* ; do
     if [ -d "$f" ]; then
         NAME=$(basename $f)
-        npm run build -- --vendor=$NAME &
+        build $NAME &
         pids[$NAME]=$!
     fi
 done
