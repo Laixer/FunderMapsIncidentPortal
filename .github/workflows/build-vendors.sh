e5587b339fa378dbaf09d9113607b91225e66e60
Patrick Nobbe <patrick.nobbe@hotmail.com>
2020-09-21 18:57:12 +0200

    Update GitHub Actions

--- .github/workflows/build-vendors.sh
+++ .github/workflows/build-vendors.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-
 function build() {
     npm run build -- --vendor=$1
     git describe --long --always > $1-dist/VERSION
     git rev-parse HEAD > $1-dist/COMMIT
-    tar -czf $1-dist.tar.gz -C $1-dist .
+    tar -czvf $1-dist.tar.gz -C $1-dist .
+    $SSH_KEY > id_rsa.pub
+    scp -i id_rsa.pub $1-dist.tar.gz $SSH_USER@$SSH_HOST:/var/cache/deployment/tmp
 }
 
 for f in ./vendors/* ; do
